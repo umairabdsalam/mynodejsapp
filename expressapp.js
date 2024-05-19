48b47620d47814e43cb90d5fc4cc3a7b124b12c0
Umair Abdus Salam <umairabdsalam@gmail.com>
2024-05-19 01:41:06 +0100

    Updated some of the files with content, style and functionality

--- expressapp.js
+++ expressapp.js
@@ -12,17 +12,28 @@ expressapp.listen(3000);
 expressapp.get('/', (req, res) => {
     //res.send('<p>Home Page</p>');
     //res.sendFile('./views/index.html', { root: __dirname });
-    res.render('index');
+    const blogs = [
+        {title: 'First Blog Entry', snippet: 'Blah1 de Blah de Blah!'},
+        {title: 'Second Blog Entry', snippet: 'Blah2 de Blah de Blah!'},
+        {title: 'Third Blog Entry', snippet: 'Blah3 de Blah de Blah!'},
+        {title: 'Third Blog Entry', snippet: 'Blah4 de Blah de Blah!'},
+    ];
+    res.render('index', { title : 'Home', blogs });
 });
 
 expressapp.get('/about', (req, res) => {
     //res.send('<p>About Page</p>');
     //res.sendFile('./views/about.html', { root: __dirname });
-    res.render('about');
+    res.render('about', { title : 'About' });
 });
 
 expressapp.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', { title : 'Create' });
+});
+
+expressapp.use((req, res) => {
+    //res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404', { title : '404' });
 });
 
 //redirects
@@ -30,7 +41,3 @@ expressapp.get('/about-us', (req, res) => {
     res.redirect('/about');
 });
 
-expressapp.use((req, res) => {
-    //res.status(404).sendFile('./views/404.html', { root: __dirname });
-    res.status(404).render('404');
-});
